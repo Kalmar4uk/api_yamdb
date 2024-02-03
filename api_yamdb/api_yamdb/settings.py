d41e0e53fc2bd23aed8af7a70b80788f8b6cfdc6
Дмитрий Лазарев <lyzarev@yandex.ru>
2024-02-03 18:41:11 +0300

    много всего по пользоваетлям

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'django_filters',
     'api',
     'reviews',
 ]
@@ -110,14 +109,7 @@ STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ),
-    'DEFAULT_PAGINATION_CLASS': (
-        'rest_framework.pagination.LimitOffsetPagination',
-    ),
-    'PAGE_SIZE': 10,
+    )
 }
 
 SIMPLE_JWT = {
