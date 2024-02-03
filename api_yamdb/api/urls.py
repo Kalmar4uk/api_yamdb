8088183e302163eeac1bd839ddf2c58b548051c2
Дмитрий Лазарев <lyzarev@yandex.ru>
2024-02-03 18:24:08 +0300

    много всего по пользоваетлям

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, GengreViewSet, TitleViewSet
+from .views import (APIGetToken, APISignup, CategoryViewSet,
+                    GengreViewSet, TitleViewSet, UsersViewSet)
 
 router_v1 = DefaultRouter()
 router_v1.register(r'api/v1/titles', TitleViewSet)
 router_v1.register(r'api/v1/categories', CategoryViewSet)
 router_v1.register(r'api/v1/genres', GengreViewSet)
+router_v1.register(
+    'users',
+    UsersViewSet,
+    basename='users'
+)
 
 urlpatterns = [
-    path('', include(router_v1.urls))
+    path('v1/auth/token/', APIGetToken.as_view(), name='get_token'),
+    path('', include(router_v1.urls)),
+    path('v1/auth/signup/', APISignup.as_view(), name='signup'),
 ]
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,76 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from .validators import validate_username
+
+USER = 'user'
+ADMIN = 'admin'
+MODERATOR = 'moderator'
+
+ROLE_CHOICES = [
+    (USER, USER),
+    (ADMIN, ADMIN),
+    (MODERATOR, MODERATOR),
+]
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        validators=(validate_username,),
+        max_length=150,
+        unique=True,
+        blank=False,
+        null=False
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        blank=False,
+        null=False
+    )
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=USER,
+        blank=True
+    )
+    bio = models.TextField(
+        blank=True,
+    )
+    first_name = models.CharField(
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        max_length=150,
+        blank=True
+    )
+    confirmation_code = models.CharField(
+        max_length=255,
+        null=True,
+        blank=False,
+        default='XXXX'
+    )
+
+    @property
+    def is_user(self):
+        return self.role == USER
+
+    @property
+    def is_admin(self):
+        return self.role == ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == MODERATOR
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
 
 
 class Genre(models.Model):
