f90e6b3e8deda645249b1db59f623338d511f48f
Роман <bespalovroma1995@gmail.com>
2024-02-08 01:44:35 +0300

    improved adminzone for models title, category and genre

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -2,9 +2,24 @@ from django.contrib import admin
 
 from .models import Category, Comment, Genre, Review, Title, User
 
-admin.site.register(Category)
+
 admin.site.register(Comment)
-admin.site.register(Genre)
-admin.site.register(Title)
 admin.site.register(Review)
 admin.site.register(User)
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'category')
+    list_filter = ('year', 'category')
+    search_fields = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
